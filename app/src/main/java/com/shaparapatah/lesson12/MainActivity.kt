b3a477291383a32791826c8b56df7b694e2d27f9
Shaparapatah <mm.fedorchenko@gmail.com>
2021-10-24 23:51:53 +0300

    12 Урок. Закончил. Прописал buildTypes. Вынес в файл properties пароли и всё, что Вы говорили. Аккаунт разработчика ещё не покупал на GooglePlay.. Добавил файл "properties", чтобы Вам было легче проверять дз.

--- app/src/main/java/com/shaparapatah/lesson12/MainActivity.kt
+++ app/src/main/java/com/shaparapatah/lesson12/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.shaparapatah.lesson12
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).text = BuildConfig.BUILD_TYPE
     }
 }
